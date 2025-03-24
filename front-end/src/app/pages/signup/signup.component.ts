fdac961f3111dc1e3145352ae1c15dd4683e7e72
ArthurToso <arthurtoso2003@gmail.com>
2025-03-23 23:50:20 -0300

    feat(signup): implementação dos campos signup

--- front-end/src/app/pages/signup/signup.component.ts
+++ front-end/src/app/pages/signup/signup.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
+
 export class SignupComponent {
+  signupForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
+    this.signupForm = this.formBuilder.group({
+      cep: [''],
+      rua: [''],
+      cidade: [''],
+      estado: ['']
+    });
+  }
+   
+  buscarCep() {
+    const cep	= this.signupForm.get('cep')?.value;
+    this.http.get(`https://viacep.com.br/ws/${cep}/json/`).subscribe((dados: any) => {
+      if (!dados.erro) {
+        this.signupForm.patchValue({
+          rua: dados.logradouro,
+          cidade: dados.localidade,
+          estado: dados.uf
+        });
+      } else {
+        alert('CEP não encontrado');
+      }
+    });
+  }
+
+  onSignup() {
+    const formData = this.signupForm.value;
+    console.log('Enviando: ', formData);
+
+    this.http.post('http://localhost:3000/signup', formData).subscribe(response => {
+      console.log('Resposta do servidor: ', response);
+    }, error => {
+      console.error('Erro ao enviar dados: ', error);
+    });
+    
+  }
 
 }
